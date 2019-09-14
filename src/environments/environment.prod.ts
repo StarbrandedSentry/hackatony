6ed86b0bddfaaf6cc31fff6b2935cb7f8fa7b7d0
StarbrandedSentry <hierarchhero@gmail.com>
2019-09-15 02:39:36 +0800

    ]p
    khhhk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDm5O4u5yRgPVtA8TINmE6lx-POvDYNsiQ",
+    authDomain: "hacka-5f19d.firebaseapp.com",
+    databaseURL: "https://hacka-5f19d.firebaseio.com",
+    projectId: "hacka-5f19d",
+    storageBucket: "hacka-5f19d.appspot.com",
+    messagingSenderId: "580814454466",
+    appId: "1:580814454466:web:cfa567de2ec88589c1855b"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDm5O4u5yRgPVtA8TINmE6lx-POvDYNsiQ",
+    authDomain: "hacka-5f19d.firebaseapp.com",
+    databaseURL: "https://hacka-5f19d.firebaseio.com",
+    projectId: "hacka-5f19d",
+    storageBucket: "hacka-5f19d.appspot.com",
+    messagingSenderId: "580814454466",
+    appId: "1:580814454466:web:cfa567de2ec88589c1855b"
+  }
 };
 
 /*
