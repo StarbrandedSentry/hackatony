86697b0e357226ac3a30bd7cb52f399e785e4584
StarbrandedSentry <hierarchhero@gmail.com>
2019-09-15 03:15:10 +0800

    fwafwafwaf

--- src/app/items/shared/items.service.ts
+++ src/app/items/shared/items.service.ts
@@ -13,5 +13,16 @@ export class ItemsService {
 
   constructor(public afs: AngularFirestore) { 
     this.itemCollection = this.afs.collection('item', ref => ref.orderBy('name', 'asc'));
+
+    this.items = this.itemCollection.snapshotChanges().pipe(map(actions => actions.map(a => {
+      const data = a.payload.doc.data() as Item;
+      data.id = a.payload.doc.id;
+
+      return data;
+    })));
+  }
+
+  getItems(){
+    return this.items;
   }
 }
