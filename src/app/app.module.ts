9004a850394cf3fa43e60499879cc187c5cede42
Ciaossuuu <55319885+Ciaossuuu@users.noreply.github.com>
2019-09-17 23:30:38 +0800

    main nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,14 @@ import { BurgerComponent } from './burger/burger.component';
 import { environment } from 'src/environments/environment.prod';
 
 import { ItemsService } from './items/shared/items.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -24,14 +32,22 @@ import { ItemsService } from './items/shared/items.service';
     ItemAddComponent,
     ItemListComponent,
     HttpComponent,
-    BurgerComponent
+    BurgerComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [ItemsService],
   bootstrap: [AppComponent]
